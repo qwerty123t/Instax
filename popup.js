a2403473a5f279adb643dd8b6011cc60884e5ed6
ronokiri <ROMANOVKIRILL12@GMAIL.COM>
2021-10-24 23:49:15 +0300

    arrows in modal menu now can rotate
    display none for modal menu on mobile devices

--- popup.js
+++ popup.js
@@ -23,18 +23,32 @@ document.addEventListener('keydown', function (evt) {
   if (evt.keyCode === 27) {
     popup.classList.remove('nav-top__alternative--show'); } });
 
-
+ 
 /* в попап меню позволяет скрыть/показать списки товаров при клике на subheader */
   let miniFilmHeader =   document.querySelector('.nav-top__hidden-subheader--alternative');
   let squareFilmHeader = document.querySelector('.nav-top__hidden-subheader--alternative:nth-of-type(3)');
   let wideFilmHeader =   document.querySelector('.nav-top__hidden-subheader--alternative:nth-of-type(4)');
+
+  let miniFilmHeaderSvg =   document.querySelector('.nav-top__hidden-subheader--alternative__svg');
+  let squareFilmHeaderSvg = document.querySelector('.nav-top__hidden-subheader--alternative:nth-of-type(3) .nav-top__hidden-subheader--alternative__svg');
+  let wideFilmHeaderSvg =   document.querySelector('.nav-top__hidden-subheader--alternative:nth-of-type(4) .nav-top__hidden-subheader--alternative__svg');
   let miniFilmList =     document.querySelector('.nav-top__list--alternative:nth-of-type(1)');
   let squareFilmList =   document.querySelector('.nav-top__list--alternative:nth-of-type(2)');
   let wideFilmList =     document.querySelector('.nav-top__list--alternative:nth-of-type(3)');
-    
+  
   miniFilmHeader.onclick = function() {
-  miniFilmList.classList.toggle('hidden');  }
+  miniFilmList.classList.toggle('hidden');  
+  miniFilmHeaderSvg.classList.toggle('rotate');}
+
   squareFilmHeader.onclick = function() {
-  squareFilmList.classList.toggle('hidden');  }
+  squareFilmList.classList.toggle('hidden');  
+  squareFilmHeaderSvg.classList.toggle('rotate');}
+
   wideFilmHeader.onclick = function() {
-  wideFilmList.classList.toggle('hidden');  }
+  wideFilmList.classList.toggle('hidden');  
+  wideFilmHeaderSvg.classList.toggle('rotate');}
+
+
+/*
+  .nav-top__hidden-subheader--alternative::after
+  */
