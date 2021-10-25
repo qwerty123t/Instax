57739a1022d9badce1162403d08d3d45e95f559f
ronokiri <ROMANOVKIRILL12@GMAIL.COM>
2021-10-25 17:13:45 +0300

    menu3 now can hide by clicking

---
+++ menu3.js
@@ -0,0 +1,14 @@
+let navBottomMenu3Header = document.querySelector('.nav-bottom__menu3_header');
+let navBottomMenu3HeaderSvg = document.querySelector('.nav-bottom__menu3_header--svg');
+let subMenus = document.querySelectorAll('.sub-menu');
+
+navBottomMenu3Header.addEventListener('click', function () { 
+  
+  /* скрыть содержимое меню3 */
+    for (var n = 0; n < subMenus.length; n++) {
+    var subMenu = subMenus[n];
+    subMenu.classList.toggle('hidden');   
+  } 
+  /* повернуть стрелочку */
+ navBottomMenu3HeaderSvg.classList.toggle('rotate');
+ });
